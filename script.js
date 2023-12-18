6e3d66164391e20a18b8c9f41be370c73e46a212
ogasawarayui <ogasawara.yui@outlook.com>
2023-12-18 22:38:05 +0900

    js9

--- script.js
+++ script.js
@@ -7,14 +7,21 @@ if (boolean) {
   resultText = "Bの処理"
 };
 console.log(resultText);
-
 var resultText = boolean ? "Aの処理" : "Bの処理";
-
 /* Q2  代入された変数の中が5より大きい時にtrueの処理と、5より小さい時にfalseの処理を作ってください。*/
 var resultNum = 6 > 5 ? true : false;
+console.log(resultNum);
 /* Q3 代入された変数の中が5より小さい時にtrueの処理と、5より小さい時にfalseの処理を作ってください。*/
 var resultNum = 4 > 5 ? true : false;
+console.log(resultNum);
 /* Q4 代入された変数の中が10以上の時にtrueの処理と、10以下の時にfalseの処理を作ってください。*/
 var resultNum = 10 >= 10 ? true : false;
+console.log(resultNum);
 /* Q5 代入された変数の中に値が存在する場合はtrue、存在しない場合はfalseの処理にしてください。*/
-var resultNum = (!0) ? true : false;
+var str = "ABC"
+var resultNum = str === null ? true : false;
+console.log(resultNum);
+/*NOTでひっくり返す場合*/
+var str = "ABC"
+var resultNum = !str ? true : false;
+console.log(resultNum);

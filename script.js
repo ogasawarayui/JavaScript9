c3d7eccf2cf7c61f6a58bd23e4a7e867d36c319f
ogasawarayui <ogasawara.yui@outlook.com>
2023-12-21 22:19:38 +0900

    js９

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 /* Q1 変数の中にtrueまたは、falseが代入された時の処理を作ってください。*/
-var boolean = true;
+/*var boolean = true;
 var resultText = "";
 if (boolean) {
   resultText = "Aの処理"
 } else {
   resultText = "Bの処理"
 };
-console.log(resultText);
+console.log(resultText);*/
 var resultText = boolean ? "Aの処理" : "Bの処理";
 /* Q2  代入された変数の中が5より大きい時にtrueの処理と、5より小さい時にfalseの処理を作ってください。*/
 var resultNum = 6 > 5 ? true : false;
 console.log(resultNum);
-/* Q3 代入された変数の中が5より小さい時にtrueの処理と、5より小さい時にfalseの処理を作ってください。*/
-var resultNum = 4 > 5 ? true : false;
+/* Q3 代入された変数の中が5より小さい時にtrueの処理と、5より大きい時にfalseの処理を作ってください。*/
+var resultNum = 4 < 5 ? true : false;
 console.log(resultNum);
 /* Q4 代入された変数の中が10以上の時にtrueの処理と、10以下の時にfalseの処理を作ってください。*/
 var resultNum = 10 >= 10 ? true : false;
